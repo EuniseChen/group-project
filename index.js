05ebbcd62c3990b1b94e4475e336aa27ec7f1de2
Eunise <eunisechen@gmail.com>
2021-11-17 01:14:45 +0800

    simplify javascript

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// 滑鼠一開始滾動，navbar的樣式就發生改變
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('.nav')
   const logoItem = document.querySelector('.logo-item')
@@ -13,29 +15,25 @@ window.addEventListener('scroll', () => {
   }
 })
 
+// 店內環境區塊的放大效果
 const fullScreen_1 = document.querySelector('.full-screen-1')
 const fullScreen_2 = document.querySelector('.full-screen-2')
 const fullScreen_3 = document.querySelector('.full-screen-3')
 const closeBtn = document.querySelector('.close-btn')
 
-fullScreen_1.addEventListener('click', () => {
+// 點擊圖片1/2/3,就會打開full-screen
+fullScreen_1.addEventListener('click', () => openModal(3))
+fullScreen_2.addEventListener('click', () => openModal(2))
+fullScreen_3.addEventListener('click', () => openModal(3))
+
+closeBtn.addEventListener('click', () => mask.style.display = 'none')
+
+// 打開full-screen的函數
+function openModal (img) {
   const mask = document.querySelector('#mask')
   const modal = document.querySelector('#modal')
+
   mask.style.display = 'block'
-  modal.style.backgroundImage = `url(../images/店家環境/shop03.JPG)`
-})
-fullScreen_2.addEventListener('click', () => {
-  const mask = document.querySelector('#mask')
-  const modal = document.querySelector('#modal')
-  mask.style.display = 'block'
-  modal.style.backgroundImage = `url(../images/店家環境/shop02.JPG)`
-})
-fullScreen_3.addEventListener('click', () => {
-  const mask = document.querySelector('#mask')
-  const modal = document.querySelector('#modal')
-  mask.style.display = 'block'
-  modal.style.backgroundImage = `url(../images/店家環境/shop03.JPG)`
-})
-closeBtn.addEventListener('click', () => {
-  mask.style.display = 'none'
-})
+
+  modal.style.backgroundImage = `url(../images/店家環境/shop0${img}.JPG)`
+}
